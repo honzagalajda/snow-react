c5d3ad4173aee5975a3ed5cc0b732fbdbc1d21dd
honzagalajda <honza.galajda@seznam.cz>
2021-12-06 21:06:57 +0100

    snow-react 1.0.0, There is snow everywhere

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+const Snow = () => {
+    return (
+        <div class="winter">
+
+            <div class="snow snow--near"></div>
+            <div class="snow snow--near snow--alt"></div>
+
+            <div class="snow snow--mid"></div>
+            <div class="snow snow--mid snow--alt"></div>
+
+            <div class="snow snow--far"></div>
+            <div class="snow snow--far snow--alt"></div>
+        </div>
+    )
+}
+
+export default Snow;
