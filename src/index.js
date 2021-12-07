e9b815ca96a455663d26519ce241f64fcf1f8b43
honzagalajda <honza.galajda@seznam.cz>
2021-12-07 21:04:30 +0100

    snow-react 1.0.1, zIndex update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './index.css';
 
-const Snow = () => {
+const Snow = ({ zIndex }) => {
+    window.onload = () => {
+        if (zIndex) {
+            const docs = document.getElementsByClassName('winter');
+            for (let i = 0; i < docs.length; i++) {
+                docs.item(i).style.zIndex = zIndex;
+            }
+        }
+    }
+
     return (
-        <div class="winter">
+        <div className="winter">
 
-            <div class="snow snow--near"></div>
-            <div class="snow snow--near snow--alt"></div>
+            <div className="snow snow--near"></div>
+            <div className="snow snow--near snow--alt"></div>
 
-            <div class="snow snow--mid"></div>
-            <div class="snow snow--mid snow--alt"></div>
+            <div className="snow snow--mid"></div>
+            <div className="snow snow--mid snow--alt"></div>
 
-            <div class="snow snow--far"></div>
-            <div class="snow snow--far snow--alt"></div>
+            <div className="snow snow--far"></div>
+            <div className="snow snow--far snow--alt"></div>
         </div>
     )
 }
