e3800386f1291d89413cd3287f69cb24bd81a4f7
honzagalajda <honza.galajda@seznam.cz>
2021-12-09 18:21:13 +0100

    snow-react 1.0.2, next z-index update

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# snow-react
+# React Snow
+React components that creates falling snowflakes on your site.
+## Installation
+With npm
+```
+npm i snow-react
+```
+## Usage
+Basic properties how to render snowflakes.
+```jsx
+import React from 'react'
+import Snow from 'snow-react';
+
+function App() {
+  return (
+    <div>
+      <Snow />
+    </div>
+  );
+}
+export default App;
+```
+
+## Configuration
+You can set specific z-index for every snowflakes.
+```jsx
+<Snow 
+    //z-index for whole element
+    zIndex='1'
+    //z-index for near snowflakes
+    zNear='2'
+    //z-index for middle snowflakes
+    zMid='2'
+    //z-index for far snowflakes
+    zFar='1'
+/>
+```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './index.css';
 
-const Snow = ({ zIndex }) => {
+const Snow = ({ zIndex, zNear, zMid, zFar }) => {
     window.onload = () => {
         if (zIndex) {
             const docs = document.getElementsByClassName('winter');
             for (let i = 0; i < docs.length; i++) {
                 docs.item(i).style.zIndex = zIndex;
+            }
+        }
+        if (zNear) {
+            const near = document.getElementsByClassName('snow--near');
+            for (let i = 0; i < near.length; i++) {
+                near.item(i).style.zIndex = zNear;
+            }
+        }
+        if (zMid) {
+            const mid = document.getElementsByClassName('snow--mid');
+            for (let i = 0; i < mid.length; i++) {
+                mid.item(i).style.zIndex = zMid;
+            }
+        }
+        if (zFar) {
+            const far = document.getElementsByClassName('snow--far');
+            for (let i = 0; i < far.length; i++) {
+                far.item(i).style.zIndex = zFar;
             }
         }
     }
